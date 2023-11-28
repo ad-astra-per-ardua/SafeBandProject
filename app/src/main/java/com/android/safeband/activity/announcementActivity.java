0ecbf0d18d245c307a3d9526439f7cb8d67a0166
한상희 <tkdgml822@gmail.com>
2023-11-28 12:36:56 +0900

    announcementActivity 추가

---
+++ app/src/main/java/com/android/safeband/activity/announcementActivity.java
@@ -0,0 +1,33 @@
+package com.android.safeband.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.android.safebandproject.R;
+
+public class announcementActivity extends AppCompatActivity {
+
+    private ImageButton backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_announcement);
+
+        ImageButton ImageButton = findViewById(R.id.backButton);
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(); // 인텐트 객체 생성하고
+                intent.putExtra("name", "suzin"); // 인텐트 객체에 데이터 넣기
+
+                setResult(RESULT_OK, intent); // 응답 보내기
+                finish(); // 현재 액티비티 없애기
+            }
+        });
+    }
+}
