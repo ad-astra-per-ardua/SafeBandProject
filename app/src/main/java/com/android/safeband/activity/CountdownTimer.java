bbb9a76232e07363f19e6238fa1484bae99dfc0e
한상희 <tkdgml822@gmail.com>
2023-12-01 18:52:07 +0900

    30초 카운터 class 및 레이아웃 생성

---
+++ app/src/main/java/com/android/safeband/activity/CountdownTimer.java
@@ -0,0 +1,16 @@
+package com.android.safeband.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.safebandproject.R;
+
+public class CountdownTimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown_timer);
+    }
+}
