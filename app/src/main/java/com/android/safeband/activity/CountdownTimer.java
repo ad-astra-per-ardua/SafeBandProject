6e6021a57d07591817c013b15b86683638edd57f
한상희 <tkdgml822@gmail.com>
2023-12-01 20:08:02 +0900

    전화걸기 및 타이머 기능 구현

--- app/src/main/java/com/android/safeband/activity/CountdownTimer.java
+++ app/src/main/java/com/android/safeband/activity/CountdownTimer.java
@@ -1,13 +1,19 @@
 package com.android.safeband.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.safebandproject.R;
 
@@ -18,8 +24,9 @@ public class CountdownTimer extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
 
     private CountDownTimer countDownTimer;
-
     long secondsRemaining;
+    // 권한 요청을 식별 하기 위해 쓰이는 변수
+    final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class CountdownTimer extends AppCompatActivity {
         timerTextView = findViewById(R.id.timerTextView);
         endButton = findViewById(R.id.endButton);
         mediaPlayer = MediaPlayer.create(this, R.raw.sound); // 사운드 변수
+
 
         // 사운드 시작
         startSound();
@@ -46,6 +54,9 @@ public class CountdownTimer extends AppCompatActivity {
 
             mediaPlayer.setLooping(false);
             mediaPlayer.pause();
+
+            // 현재 액티비티를 종료 후 이전 화면으로 돌아간다.
+            finish();
         });
     }
 
@@ -79,6 +90,9 @@ public class CountdownTimer extends AppCompatActivity {
             @Override
             public void onFinish() {
                 timerTextView.setText("0");
+
+                // 타이머 0이 되면 전화를 건다
+                makeCall();
             }
         }.start();
     }
@@ -88,5 +102,46 @@ public class CountdownTimer extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mediaPlayer.release();
+    }
+
+    private void makeCall() {
+        String phoneNumber = "1234567890"; // 여기에 전화번호를 입력하세요
+
+        // 현재 전화 걸기 권한의 상태
+        int permissionCheck = ContextCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE);
+
+        // 권한을 허용 받았을 때
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            // 전화를 건다.
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber)));
+        }
+        // 권한을 허용 받지 못했을 때
+        else {
+            Toast.makeText(this,"권한 승인이 필요합니다",Toast.LENGTH_LONG).show();
+
+            // 사용자가 명시적으로 권한을 거부했을 때
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CALL_PHONE)) {
+                Toast.makeText(this,"전화 권한이 필요합니다.",Toast.LENGTH_LONG).show();
+            } else {
+                // 전화 걸기 권한을 다시 요청한다. 뒤에 상수는 권한 요청 식별할 때 쓰입니다.
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+            }
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                // 사용자가 권한을 허용했을 경우
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makeCall(); // 권한을 얻었으니 전화를 건다.
+                } else {
+                    Toast.makeText(this, "전화 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
     }
 }
